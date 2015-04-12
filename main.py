be33957e9526f2ad9375130ddab455f6f891ec30
metaregress <metaregress@gmail.com>
2015-04-12 16:57:34 -0700

    Initial commit. Get outta my face, Git.

---
+++ main.py
@@ -0,0 +1,13 @@
+#A program to generate random D&D dungeons
+
+import random
+
+#room class
+class Room:
+    pass
+#passage class
+class Passage:
+    pass
+    
+if __name__ == "__main__":
+    print("it works")
