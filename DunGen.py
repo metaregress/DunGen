1a7fcea3e425bf6e7b6e7712d945320e3d39f60c
metaregress <metaregress@gmail.com>
2015-04-13 00:00:35 -0700

    It's alive!
    Got the main loop in place thanks to Chamber.getRandChild().
    
    Added .gitignore.

--- DunGen.py
+++ DunGen.py
@@ -10,6 +10,17 @@ class Chamber:
         self.width = width
         self.passage_count = passage_count
         self.passages = []
+        self.parent = None
+     
+    def  getRandChild(self):
+        print("seeking random child chamber to build")
+        if len(self.passages) == self.passage_count:
+            print("found full chamber: " + str(len(self.passages)) + " out of " + str(self.passage_count) + " in use")
+            passage = random.choice(self.passages)
+            chamber = random.choice(passage.chambers)
+            return chamber.getRandChild()
+        else:
+            return self
         
     def __str__(self, level=0):
         return_string = "\t" * level + "A chamber of dimensions " + str(self.length) + " by " + str(self.width) + "\n"
@@ -24,6 +35,7 @@ class Passage:
         self.width = width
         #TODO: Passages currently just lead to chambers, make them able to branch etc.
         self.chambers = []
+        self.parent = None
     
     def __str__(self, level=0):
         return_string = "\t" * level + "A corridor of dimensions " + str(self.length) + " by " + str(self.width) + "\n"
@@ -41,6 +53,8 @@ def generateChamber():
     chamber_collection = []
     chamber_collection.append(Chamber(10,10,1))
     chamber_collection.append(Chamber(20, 20, 2))
+    chamber_collection.append(Chamber(20, 30, 2))
+    chamber_collection.append(Chamber(40, 40, 2))
     return random.choice(chamber_collection)
 
 def generatePassage():
@@ -56,7 +70,6 @@ def generatePassage():
     #TODO: can probably make these into json blobs or something for more sustainable storage
     passage_collection = []
     passage_collection.append(Passage(20, 10))
-    passage_collection.append(Passage(10, 10))
     passage_collection.append(Passage(30, 10))
     passage_collection.append(Passage(20, 5))
     return random.choice(passage_collection)
@@ -64,21 +77,36 @@ def generatePassage():
     
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python main.py [max_rooms] [num_levels]")
+        print("Usage: python DunGen.py [max_rooms] [num_levels]")
         exit()
-    max_rooms = sys.argv[1]
-    num_levels = sys.argv[2]
-    print("creating dungeon with up to " + max_rooms + " rooms")
-    starting_areas = [Chamber(20, 20, 2), Chamber(10, 10, 2)]
+    max_chambers = int(sys.argv[1])
+    num_levels = int(sys.argv[2])
+    print("creating dungeon with up to " + str(max_chambers) + " chambers")
+    starting_areas = [Chamber(20, 20, 2), Chamber(30, 30, 2)]
     starting_area = random.choice(starting_areas)
+    chambers = 1
     
     for p in range(starting_area.passage_count):
-        #breadth first or depth first...?
+        if chambers < int(max_chambers):
+            passage = generatePassage()
+            starting_area.passages.append(passage)
+            passage.parent = starting_area
+    
+    for passage in starting_area.passages:
+        chamber = generateChamber()
+        passage.chambers.append(chamber)
+        chamber.parent = passage
+        chambers += 1
+     
+    while chambers < max_chambers:
+        rand_chamber = starting_area.getRandChild()
         passage = generatePassage()
-        starting_area.passages.append(passage)
-    
-    #for passage in starting_area.passages:
-    
+        rand_chamber.passages.append(passage)
+        passage.parent = rand_chamber
+        child_chamber = generateChamber()
+        passage.chambers.append(child_chamber)
+        child_chamber.parent = passage
+        chambers += 1
 
     print(starting_area)
         
